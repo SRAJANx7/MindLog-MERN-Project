1d35f9fcd733a433bc5744d6c4d72565658cac2b
Srajan <123717381+SRAJANx7@users.noreply.github.com>
2025-06-16 03:15:33 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,25 @@ const app = express();
 require("dotenv").config();
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
+import cors from 'cors';
 const cors = require("cors");
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://mind-log-mern-project.vercel.app'
+];
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: https://mind-log-mern-project.vercel.app/, credentials: true }));
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
